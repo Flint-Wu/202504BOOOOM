458a10671fefcb1d5154530e18cc0772209cc595
StormDDD <132730019+771217832@users.noreply.github.com>
2025-04-19 16:41:06 +0800

    0419更新3
    实现了WindRegion控制草的运动，加入了下雪vfx

--- Assets/Script/environment/WindRegion.cs
+++ Assets/Script/environment/WindRegion.cs
@@ -14,11 +14,16 @@ public class WindRegion : MonoBehaviour
     [Header("玩家进入风区的体力的消减百分比")]
     public float PlayerEnterWindStrengthPer;//记录玩家进入风区的的体力,百分比
     public float WindTime = 0;//开始时间
+    public Material[] grassMaterial;
+    public GlobalGrassRenderer grassRenderer;
+    public bool isPlayerIn;
+    private GameObject player;
     void Start()
     {
         WindTime = 0;
         //设置vfx的边界和风区的大小一致
         WindVfx.transform.localScale = this.GetComponent<BoxCollider>().size;
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
@@ -32,29 +37,42 @@ public class WindRegion : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            PlayerEnterPhysicalStrength = other.GetComponent<PlayerPhysicalStrength>().currentPhysicalStrength;
-            float maxPhysicalStrength = other.GetComponent<PlayerPhysicalStrength>().maxPhysicalStrength;
-            other.GetComponent<PlayerPhysicalStrength>().stopRecovering();
-            if (PlayerEnterPhysicalStrength/maxPhysicalStrength >PlayerEnterWindStrengthPer)
-            {
-                other.GetComponent<PlayerPhysicalStrength>().currentPhysicalStrength = PlayerEnterWindStrengthPer * maxPhysicalStrength;
-            }
-            else
-            {
-                //如果玩家的体力值小于风区消减的体力值，则对玩家体力不做任何处理
-                return;
-            }
+            isPlayerIn = true;
+        }
+    }
+
+    void ReducePlayerPhysical()
+    {
+
+        PlayerEnterPhysicalStrength = player.GetComponent<PlayerPhysicalStrength>().currentPhysicalStrength;
+        float maxPhysicalStrength = player.GetComponent<PlayerPhysicalStrength>().maxPhysicalStrength;
+        player.GetComponent<PlayerPhysicalStrength>().stopRecovering();
+        if (PlayerEnterPhysicalStrength/maxPhysicalStrength >PlayerEnterWindStrengthPer)
+        {
+            player.GetComponent<PlayerPhysicalStrength>().currentPhysicalStrength = PlayerEnterWindStrengthPer * maxPhysicalStrength;
+        }
+        else
+        {
+            //如果玩家的体力值小于风区消减的体力值，则对玩家体力不做任何处理
+            return;
+        }
+        
+    }
+
+    void RecoverPlayerPhysical()
+    {
+        if (player.CompareTag("Player"))
+        {
+            player.GetComponent<PlayerPhysicalStrength>().currentPhysicalStrength = PlayerEnterPhysicalStrength;
+            //other.GetComponent<PlayerPhysicalStrength>().startRecovering();
         }
     }
 
     void OnTriggerExit(Collider other)
     {
         //离开风区时，恢复玩家的体力值
-        if (other.CompareTag("Player"))
-        {
-            other.GetComponent<PlayerPhysicalStrength>().currentPhysicalStrength = PlayerEnterPhysicalStrength;
-            //other.GetComponent<PlayerPhysicalStrength>().startRecovering();
-        }
+        isPlayerIn = false;
+
     }
 
     void InverseWind()
@@ -66,12 +84,26 @@ public class WindRegion : MonoBehaviour
                 WindVfx.SetActive(false);
                 IsWindBegin = false;
                 WindTime = 0;
+                for (int i = 0; i < grassMaterial.Length; i++)
+                {
+                    grassMaterial[i].SetFloat("_WindSpeed", 2);
+                }
+                grassRenderer.ForceRefresh();
+                if(!isPlayerIn)return;
+                RecoverPlayerPhysical();
             }
             else if (IsWindBegin == false)
             {
                 WindVfx.SetActive(true);
                 IsWindBegin = true;
                 WindTime = 0;
+                for (int i = 0; i < grassMaterial.Length; i++)
+                {
+                    grassMaterial[i].SetFloat("_WindSpeed", 12);
+                }
+                grassRenderer.ForceRefresh();
+                if(!isPlayerIn)return;
+                ReducePlayerPhysical();
             }
         }
     }
