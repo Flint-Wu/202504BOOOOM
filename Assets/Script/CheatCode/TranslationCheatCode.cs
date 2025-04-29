cc6f5ee16f2372217ed7d111a759a471f9a39ade
StormDDD <132730019+771217832@users.noreply.github.com>
2025-04-29 18:47:24 +0800

    0429 解决了重复验证码的问题

--- Assets/Script/CheatCode/TranslationCheatCode.cs
+++ Assets/Script/CheatCode/TranslationCheatCode.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
-using OpenCover.Framework.Model;
+using TMPro;
+
+// Removing System.Diagnostics to avoid ambiguity with UnityEngine.Debug
 using UnityEngine;
 
 public class TranslationCheatCode : MonoBehaviour
@@ -13,6 +15,7 @@ public class TranslationCheatCode : MonoBehaviour
     }
     public MadeConcateAll madeConcateAll;
     public List<PlayerLocMap> TotalLocunNums = new List<PlayerLocMap>();
+    public TextMeshProUGUI TextMeshProUGUI;
     public void TranslateCheatCode()
     {
         // GetComponent<ReadCode>().SetCheatCode();
@@ -21,13 +24,35 @@ public class TranslationCheatCode : MonoBehaviour
         GetComponent<ReMadeLoc>().ReTrans();
         //TotalLocunNums.Add<ReMadeLoc>().LocunNums.ToArray().ToList();
         
+        //如果HelperID重复，则不添加
+        if(TotalLocunNums.Count > 0)
+        {
+            for (int i = 0; i < TotalLocunNums.Count; i++)
+            {
+                if (TotalLocunNums[i].PlayerID == GetComponent<ReMadeID>().HelperID)
+                {
+                    Debug.LogWarning("Repeat Player CheatCode: " + TotalLocunNums[i].PlayerID + " " + TotalLocunNums[i].LocNum);
+                    TextMeshProUGUI.text = "Repeat Player CheatCode: " + TotalLocunNums[i].PlayerID;
+                    StartCoroutine(ResetWarning());
+                    return;
+                }
+            }
+        }
         //TotalLocunNums add HelperID and LocNum
         for (int i = 0; i < GetComponent<ReMadeLoc>().LocunNums.Length; i++)
         {
             PlayerLocMap playerLocMap = new PlayerLocMap();
             playerLocMap.PlayerID = GetComponent<ReMadeID>().HelperID;
             playerLocMap.LocNum = GetComponent<ReMadeLoc>().LocunNums[i];
+            
             TotalLocunNums.Add(playerLocMap);
         }
+    }
+
+    IEnumerator ResetWarning()
+    {
+        yield return new WaitForSeconds(2f);
+        TextMeshProUGUI.text = "";
+
     }
 }
